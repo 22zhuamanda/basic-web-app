c96f64f888941fde70cf6b35842ef75edd00677d
Amanda Zhu <22zhuamanda@gmail.com>
2024-03-14 16:08:14 -0400

    largest

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -29,11 +29,17 @@ export default function QueryProcessor(query: string): string {
     return sum.toString();
   }
 
-  if (query.toLowerCase().includes("largest")) {
-    return (
-      "87"
-    );
-  }
+  if (query.toLowerCase().includes("largest:")) {
+    // Extract numbers from the query
+    const match = query.match(/\d+/g);
+    if (match) {
+      const numbers = match.map(Number);
+    
+    // Find the largest number
+    const largest = Math.max(...numbers);
+  
+    return '${largest}';
+  }}
 
   if (query.toLowerCase().includes("34 plus 3")) {
     return (
